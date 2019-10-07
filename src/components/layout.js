0ceffc133cea708cdddbd4fa6a5973321189899b
Greg <thompsgj@yahoo.com>
2019-10-07 12:57:57 +0900

    Added Footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,12 +6,14 @@ import "./bootstrap.min.css"
 import "./layout.css"
 
 import Navbar from "../components/Globals/Navbar"
+import Footer from "../components/Globals/Footer"
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
       {children}
+      <Footer />
     </>
   )
 }
